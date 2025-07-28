51c9aa0478c0aa8dd07da23821cfd95e055162c2
slzp6 <suzuki.motofumi@gmail.com>
2025-07-28 12:17:49 +0900

    fix

--- ch07/q7-1.c
+++ ch07/q7-1.c
@@ -1,50 +1,49 @@
-/* code: q7-1.c   (v1.25.00) */  
+/* code: q7-1.c   (v1.25.00) */
 #include <stdio.h>
 #include <stdlib.h>
 #include <search.h>
-  
+
 #define ARRAY_SIZE 5
-#define EXTRA_ROOM 1  
-  
-/* ------------------------------------------- */ 
+#define EXTRA_ROOM 1
+
+/* ------------------------------------------- */
 int compare (const void *x, const void *y)
 {
-  int a = *(const int *) x;
-  int b = *(const int *) y;
-  return (a > b) - (a < b);
-}
+  int a = *(const int *) x;
+  int b = *(const int *) y;
+  return (a > b) - (a < b);
+}
 
- 
-/* ------------------------------------------- */ 
+/* ------------------------------------------- */
 void print_array (int arr[], size_t n)
 {
-  for (size_t i = 0; i < n; i++) {
-    printf ("%d ", arr[i]);
-  } printf ("\n");
-}  
+  for (size_t i = 0; i < n; i++) {
+    printf ("%d ", arr[i]);
+  }
+  printf ("\n");
+}
 
-/* ------------------------------------------- */ 
+/* ------------------------------------------- */
 int main ()
 {
-  int key = 25;
-  size_t elements = ARRAY_SIZE;
-  int array[ARRAY_SIZE + EXTRA_ROOM] = { 10, 20, 30, 40, 50 };
-   printf ("Initial array: ");
-  print_array (array, elements);
-   int *found = lsearch (&key, array, &elements, sizeof (int), compare);
-   if (!found) {
-    printf ("Search error\n");
-    return EXIT_FAILURE;
-  }
-   printf ("\nResult:\n");
-  if (elements == ARRAY_SIZE) {
-    printf ("Key %d was already in the array.\n", key);
-  }
+  int key = 25;
+  size_t elements = ARRAY_SIZE;
+  int array[ARRAY_SIZE + EXTRA_ROOM] = { 10, 20, 30, 40, 50 };
+
+  printf ("Initial array: ");
+  print_array (array, elements);
+  int *found = lsearch (&key, array, &elements, sizeof (int), compare);
+  if (!found) {
+    printf ("Search error\n");
+    return EXIT_FAILURE;
+  }
+  printf ("\nResult:\n");
+  if (elements == ARRAY_SIZE) {
+    printf ("Key %d was already in the array.\n", key);
+  }
   else {
-    printf ("Key %d was added to the array.\n", key);
-  }
-   print_array (array, elements);
-  return EXIT_SUCCESS;
-}
-
-
+    printf ("Key %d was added to the array.\n", key);
+  }
+  print_array (array, elements);
+  return EXIT_SUCCESS;
+}
